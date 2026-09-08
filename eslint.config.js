ea2dd8fa77ed7310e0d6cf82520d787cb049d5b5
Russell Moore <russellwmoore@gmail.com>
2026-09-08 17:00:53 -0400

    adds eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -11,12 +11,21 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      // Enforce using types instead of interfaces
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     },
   },
-])
+])  
